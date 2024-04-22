0f43b8371c89c86b2ef93b18129268e2c13673b1
Ondřej Cervan <238519@vutbr.cz>
2024-04-22 09:35:00 +0200

    commit

--- fibo.py
+++ fibo.py
@@ -1,6 +1,16 @@
+def recursice_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursice_nth_fibo(n-1) + recursice_nth_fibo(n-2)
 
 def main():
-    pass
+    n = int(input("Počet členů Fibonacciho posloupnosti: "))
+    seq = [recursice_nth_fibo(num)] for num in range(n+1)]
+    return seq
+
 
 
 if __name__ == "__main__":
